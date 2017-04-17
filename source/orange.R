b9c04b6f3f0b3b30f69b058d0995eee682c43029
Charles McCann <cmccann@Charless-MacBook-Pro.local>
2017-04-17 14:45:11 -0500

    Initial submission for orange.

---
+++ source/orange.R
@@ -0,0 +1,15 @@
+#install.packages ('doBy')
+library(doBy)
+Orange
+require(stats); require(graphics)
+coplot(circumference ~ age | Tree, data = Orange, show.given = FALSE)
+fm1 <- nls(circumference ~ SSlogis(age, Asym, xmid, scal),
+           data = Orange, subset = Tree == 3)
+plot(circumference ~ age, data = Orange, subset = Tree == 3,
+     xlab = "Tree age (days since 1968/12/31)",
+     ylab = "Tree circumference (mm)", las = 1,
+     main = "Orange tree data and fitted model (Tree 3 only)")
+age <- seq(0, 1600, length.out = 101)
+lines(age, predict(fm1, list(age = age)))
+
+summaryBy(Orange$circumference  ~ Orange$Tree, data=Orange , FUN=c(mean),na.rm=TRUE,keep.names=TRUE)
