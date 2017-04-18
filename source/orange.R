bee6ca19a158e65823aa8d1d6c00325186eec8bb
Charles McCann <cmccann@Charless-MacBook-Pro.local>
2017-04-18 14:47:03 -0500

    Added box plots for oranges.

--- source/orange.R
+++ source/orange.R
@@ -11,3 +11,14 @@ summaryBy(Orange$circumference  ~ Orange$Tree, data=Orange , FUN=c(mean,median),
 # This prints out the age vs circumference.
 # TODO: Get plot styles to change for tree.
 ggplot(Orange, aes(x = Orange$age, y = Orange$circumference)) + geom_point(size = 5)
+
+Orange$Tree <- factor(Orange$Tree, labels = c("1", "2", "3", "4", "5"))
+
+# Create colors for box plot.
+fill <- "#4271AE"
+line <- "#1F3552"
+
+# Generate box plot of circumference by tree.
+ggplot(Orange, aes(x = Tree, y = circumference)) + geom_boxplot(fill = fill, colour = line, alpha = .7) + 
+  scale_x_discrete(name = "Tree #") + scale_y_continuous(name = "Circumference in inches") +
+  ggtitle("Boxplot of circumference by tree")
