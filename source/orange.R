70206d17570f6b42fd9922bb8ccb7482d3779517
Charles McCann <cmccann@Charless-MacBook-Pro.local>
2017-04-18 14:27:21 -0500

    Added comments for orange.r

--- source/orange.R
+++ source/orange.R
@@ -1,18 +1,13 @@
 #install.packages ('doBy')
+#Load doBy and ggplot libraries to be utilized in this code.
 library(doBy)
 library(ggplot2)
+#print out Orange data which is built-in.
 Orange
-require(stats); require(graphics)
-coplot(circumference ~ age | Tree, data = Orange, show.given = FALSE)
-fm1 <- nls(circumference ~ SSlogis(age, Asym, xmid, scal),
-           data = Orange, subset = Tree == 3)
-plot(circumference ~ age, data = Orange, subset = Tree == 3,
-     xlab = "Tree age (days since 1968/12/31)",
-     ylab = "Tree circumference (mm)", las = 1,
-     main = "Orange tree data and fitted model (Tree 3 only)")
-age <- seq(0, 1600, length.out = 101)
-lines(age, predict(fm1, list(age = age)))
 
+# This prints out mean and median which are listed in FUN. This compares circumference by tree.
 summaryBy(Orange$circumference  ~ Orange$Tree, data=Orange , FUN=c(mean,median),na.rm=TRUE,keep.names=TRUE)
 
-ggplot(Orange, aes(x = Orange$age, y = Orange$circumference, shape = Orange$Tree)) + geom_point(size = 5)
+# This prints out the age vs circumference.
+# TODO: Get plot styles to change for tree.
+ggplot(Orange, aes(x = Orange$age, y = Orange$circumference)) + geom_point(size = 5)
